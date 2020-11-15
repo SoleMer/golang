ebc38ccae5264adbbccea8f8142da7b4b07c6d84
SoleMer <soledadmerino.1944@gmail.com>
2020-11-15 17:48:31 -0300

    sql

---
+++ REST/rest.go
@@ -0,0 +1,26 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main(){
+	r := gin.Default()
+	r.GET("/users/:name",getUsersHandler)
+	r.POST("/users", addUserHandler)
+	r.Run()
+
+}
+
+func addUserHandler(c *gin.Context){
+	c.JSON(201, gin.H{
+		"name":"john",
+		"lastname": "doe",
+	})
+}
+
+func getUsersHandler(c *gin.Context){
+	name := c.Param("name")
+	lastname := c.Query("lastname")
+	c.JSON(200,gin.H{
+		"name": name +" "+lastname,
+	})
+}
